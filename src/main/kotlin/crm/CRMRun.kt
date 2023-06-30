f158e44ffab0fc35769a829f3ff6d612bf1f8be3
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-30 15:01:39 +0900

    fix: Edit CRMRun
    - Add and apply NUMBER_OF_EMPLOYEES constant
    - Add isEmailConfirmed to CrmUser.new

--- src/main/kotlin/crm/CRMRun.kt
+++ src/main/kotlin/crm/CRMRun.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.deleteAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.kodein.di.instance
+import kotlin.random.Random
 
 fun crmRun() {
     val database: Database by kodeinCRM.instance(tag = "local")
@@ -16,7 +17,7 @@ fun crmRun() {
         val company = crmCompanyService.getCompany()
         println("company: ${company?.domainName}, ${company?.numberOfEmployees}")
 
-        val user = crmUserService.findById(1L)
+        val user = crmUserService.findById((1..NUMBER_OF_EMPLOYEES).random())
         println("user: ${user?.id?.value}, ${user?.email}, ${user?.type}")
 
         crmMainService.changeEmail(1, "test@email.com")
@@ -26,20 +27,23 @@ fun crmRun() {
     }
 }
 
+private const val NUMBER_OF_EMPLOYEES = 10L
+
 fun onCRMTables(database: Database, block: () -> Unit) {
     transaction(database) {
         SchemaUtils.create(CrmUsers, CrmCompanies)
 
         val companyDomainName = "test.com"
-        for (i in 1..10) {
+        for (i in 1..NUMBER_OF_EMPLOYEES) {
             CrmUser.new {
                 this.email = "user$i@$companyDomainName"
                 this.type = UserType.EMPLOYEE.name
+                this.isEmailConfirmed = Random.nextBoolean()
             }
         }
         CrmCompany.new {
             this.domainName = companyDomainName
-            this.numberOfEmployees = 10
+            this.numberOfEmployees = NUMBER_OF_EMPLOYEES
         }
 
         block()
